1cce4049dece6388e913ef020022878e1995d634
Farid <thesomeq@gmail.com>
2019-12-04 21:30:27 +0600

    Задание 1 - API.

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import add, subtract, multiply, divide
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add/', add, name='add'),
+    path('subtract/', subtract, name='subtract'),
+    path('multiply/', multiply, name='multiply'),
+    path('divide/', divide, name='divide')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,84 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+
+def is_valid(A, B):
+    if A is None or B is None:
+        return False, 'Data should contain two numbers named A and B.'
+    if type(A) not in (int, float) or type(B) not in (int, float):
+        return False, 'Both A and B should be numbers.'
+    return True, None
+
+
+def make_response(data, status_code):
+    response = JsonResponse(data)
+    response.status_code = status_code
+    return response
+
+
+@csrf_exempt
+def add(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            data = json.loads(request.body)
+            A = data.get('A', None)
+            B = data.get('B', None)
+            valid, error = is_valid(A, B)
+            if not valid:
+                return make_response({'error': error}, 400)
+            return JsonResponse({'answer': A + B})
+        else:
+            return make_response({'error': 'No data provided!'}, 400)
+    return make_response({'error': 'Method not allowed.'}, 405)
+
+
+@csrf_exempt
+def subtract(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            data = json.loads(request.body)
+            A = data.get('A', None)
+            B = data.get('B', None)
+            valid, error = is_valid(A, B)
+            if not valid:
+                return make_response({'error': error}, 400)
+            return JsonResponse({'answer': A - B})
+        else:
+            return make_response({'error': 'No data provided!'}, 400)
+    return make_response({'error': 'Method not allowed.'}, 405)
+
+
+@csrf_exempt
+def multiply(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            data = json.loads(request.body)
+            A = data.get('A', None)
+            B = data.get('B', None)
+            valid, error = is_valid(A, B)
+            if not valid:
+                return make_response({'error': error}, 400)
+            return JsonResponse({'answer': A * B})
+        else:
+            return make_response({'error': 'No data provided!'}, 400)
+    return make_response({'error': 'Method not allowed.'}, 405)
+
+
+@csrf_exempt
+def divide(request, *args, **kwargs):
+    if request.method == 'POST':
+        if request.body:
+            data = json.loads(request.body)
+            A = data.get('A', None)
+            B = data.get('B', None)
+            valid, error = is_valid(A, B)
+            if not valid:
+                return make_response({'error': error}, 400)
+            if B == 0:
+                return make_response({'error': 'Division by zero!'}, 400)
+            return JsonResponse({'answer': A / B})
+        else:
+            return make_response({'error': 'No data provided!'}, 400)
+    return make_response({'error': 'Method not allowed.'}, 405)
